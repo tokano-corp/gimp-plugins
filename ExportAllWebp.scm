a67a4caaf9142ebe2f32a13732cad2792b40e167
tokano-corp <191471969+tokano-corp@users.noreply.github.com>
2025-02-08 01:37:32 +0100

    [ADD] Export all opened images as WEBP

---
+++ ExportAllWebp.scm
@@ -0,0 +1,48 @@
+(define (script-fu-export-all-images-webp quality)
+  (let* (
+         (image-ids (cadr (gimp-image-list))) ; Retrieve opened images IDs
+         (image-list (if (vector? image-ids) (vector->list image-ids) '())) ; Convert to list
+        )
+
+    (define (replace-extension filename new-ext) ; Replace extension
+      (let* ((dot-index (string-length filename))
+         (dot-index (let loop ((i (- dot-index 1)))
+                      (if (or (= i -1) (char=? (string-ref filename i) #\.))
+                          i
+                          (loop (- i 1))
+                      )
+                    )
+         )
+         (base-name (substring filename 0 dot-index)))
+        (string-append base-name new-ext)
+      )
+    )
+
+    (define (export-image image)
+      (let* (
+             (basename (car (gimp-image-get-filename image))) ; Retrieve image path and name
+             (filename (replace-extension basename ".webp"))   ; Replace extension with .webp
+             (drawable (car (gimp-image-get-active-drawable image))) ; Retrieve active layer
+            )
+        ; Export image in WebP format with chosen quality
+        (file-webp-save 1 image drawable filename filename 0 0 quality 100 0 0 0 0 0 0 0 0 0)
+      )
+    )
+    
+    ; Export each image
+    (for-each export-image image-list)
+  )
+)
+
+; Save script
+(script-fu-register "script-fu-export-all-images-webp"
+  "Export All Images as WEBP"
+  "Export all opened images as WEBP with chosen quality"
+  "Tokano"
+  "Copyright 2025, Tokano"
+  "January 03, 2025"
+  "*"
+  SF-ADJUSTMENT "Quality" '(60 0 100 1 1 0 0) ;  Ajout du paramètre de qualité
+)
+; Add option to menu
+(script-fu-menu-register "script-fu-export-all-images-webp" "<Image>/File/Export")
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gimp-plugins
+
+A list of free Gimp plugins.
+
+Official Gimp documentation page for installing scripts https://docs.gimp.org/en/install-script-fu.html\
+For install a plugin, copy the file in Gimp scripts folder.
+
+Example: `C:\Users\tokano\AppData\Roaming\GIMP\2.10\scripts`
+
+## ExportAllWebp
+Export all opened images as WEBP with chosen quality.\
+- A new option **Export All Images as WEBP** as created in menu **File/Export**
+- Default quality is **60%**
+- Save with the same **name**
+- Save on the same **location**
